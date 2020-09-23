9babc9052828c7d1cbe2d537e32c854cb84e3477
mitrofin <mitrofn@gmail.com>
2020-09-23 23:22:25 +0300

    add task

---
+++ task-10.js
@@ -0,0 +1,13 @@
+const getSortedUniqueSkills = array => array;
+
+console.log(getSortedUniqueSkills(users));
+/* [ 'adipisicing', 'amet',
+ 'anim', 'commodo',
+ 'culpa', 'elit',
+ 'ex', 'ipsum',
+ 'irure', 'laborum',
+ 'lorem', 'mollit',
+ 'non', 'nostrud',
+ 'nulla', 'proident',
+ 'tempor', 'velit',
+ 'veniam' ]; */
