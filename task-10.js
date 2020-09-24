58b134e8bcad970e5a2e79a4935182013ffedd14
mitrofin <mitrofn@gmail.com>
2020-09-24 20:29:17 +0300

    Update task-10.js

--- task-10.js
+++ task-10.js
@@ -1,6 +1,11 @@
-const getSortedUniqueSkills = array => array;
+const getSortedUniqueSkills = array =>
+  array
+    .reduce((acc, user) => [...acc, ...user.skills], [])
+    .filter((current, index, arr) => arr.indexOf(current) === index)
+    .sort();
 
 console.log(getSortedUniqueSkills(users));
+/* [ 'adipisicing', 'amet',
 /* [ 'adipisicing', 'amet',
  'anim', 'commodo',
  'culpa', 'elit',
@@ -11,3 +16,21 @@ console.log(getSortedUniqueSkills(users));
  'nulla', 'proident',
  'tempor', 'velit',
  'veniam' ]; */
+
+/**
+ *reduce, filter, sort
+Получи массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+Слияние массивов:
+
+const odd = [1, 3, 5];
+const even = [2, 4, 6];
+
+// 1
+[...odd, ...even];
+//  [1, 3, 5, 2, 4, 6]
+
+// 2
+odd.concat(even)
+//  [1, 3, 5, 2, 4, 6]
+ */
